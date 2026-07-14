5f2c5c40adee0d5bc8fd0701e3fb39d51d82c43f
Apoorv Raizada <apoorvvraizadaa@gmail.com>
2026-07-14 10:35:20 +0530

    Time: 17 ms (98.16%), Space: 26.3 MB (86.41%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -2,3 +2,11 @@ class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         nums.sort()
         return nums
+        # pivot = nums[right]
+        # i = left - 1 
+        # for j in range(left, right):
+        #     if nums[j] < pivot:
+        #         i += 1
+        #         nums[i], nums[j] = nums[j], nums[i] # Swap smaller element forward
+        #         # Move pivot to its correct final position
+        #         nums[i + 1], nums[right] = nums[right], nums[i + 1]
